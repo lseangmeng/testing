cc69b37dafdc19773103d872a737e1da90d3a0fd
lseangmeng <lseangmeng@gmail.com>
2022-06-26 07:55:00 +0700

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,19 @@
+[
+	{
+		"name": "Admin",
+		"collapse": true,
+		"templates": [
+			{
+				"name": "Leave",
+				"inputs": [
+					{
+						"name": "name",
+						"value": "Dara",
+					},
+				],
+				"content": "Leave form from [name] this is a leave form"
+			}
+		]
+		
+	}
+] 
