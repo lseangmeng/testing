388f76a298c7c7e9299ccfa49e7c66b843a26bc1
lseangmeng <lseangmeng@gmail.com>
2022-06-26 07:58:02 +0700

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,36 @@
+[
+	{
+		"name": "MEF",
+		"collapse": true,
+		"templates": [
+			{
+				"name": "Leave",
+				"inputs": [
+					{
+						"name": "name",
+						"value": "Dara",
+					},
+				],
+				"content": "Leave form from [name] this is a leave form"
+			}
+		]
+		
+	},
+	{
+		"name": "MOC",
+		"collapse": false,
+		"templates": [
+			{
+				"name": "Overtime",
+				"inputs": [
+					{
+						"name": "name",
+						"value": "Sok",
+					},
+				],
+				"content": "Overtime form from [name] this is a leave form"
+			}
+		]
+		
+	}
+] 
